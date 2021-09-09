a55a8fdcf62b7bd91c7bb069d94fcc99021666b8
vladyslavdmytrenko <vladyslavdmytrenko96@gmail.com>
2021-09-09 16:35:26 +0300

    added search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 import "./style.css";
 
+import { NoteContext } from "../../context/NoteContext";
+
 function Search() {
+  const { notes } = useContext(NoteContext);
+  const [ focus, setFocus ] = useState(false);
+  const [ searchArr, setSearchArr ] = useState([]);
+
+  const handlerChange = e => {
+    console.log(e.target.value);
+    if (!e.target.value.length) setSearchArr([]);  
+    const filter = notes.filter(i => i.title.includes(e.target.value));
+    console.log(filter);
+    setSearchArr(filter);
+  }
+
   return (
     <div className="header-search">
       <input
@@ -11,7 +26,24 @@ function Search() {
         id="search"
         name="search"
         placeholder="Search"
+        onChange={ handlerChange }
+        onFocus={ e => setFocus(true)}
       />
+      <div className={`drop-down-list ${!focus ? 'hide': ''}`}>
+        <ul>
+          {searchArr.length
+            ? searchArr.map((i) => {
+                return (
+                  <li key={i.id}>
+                    <NavLink to={`/${i.id}`} activeClassName="active">
+                      {i.title}
+                    </NavLink>
+                  </li>
+                );
+              })
+            : null}
+        </ul>
+      </div>
     </div>
   );
 }
